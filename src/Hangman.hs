6fd17c0ade54155ac64b7f62d7edd9329c602f35
Justin Le <justin@jle.im>
2014-07-05 18:42:41 -0700

    done with new version; but a bit verbose. hm.

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -2,23 +2,19 @@
 
 module Main (main) where
 
--- import Control.Arrow
--- import Control.Auto.Effects
--- import Control.Auto.Event.Internal
--- import Control.Auto.Generate
--- import Control.Auto.Time
--- import Data.Functor
 import Control.Auto
 import Control.Auto.Event
+import Control.Auto.Process.Random
 import Control.Auto.Run
 import Control.Auto.Switch
 import Control.Monad
 import Data.Char
 import Data.List
 import Data.Maybe
-import Prelude hiding                 ((.), id)
+import Prelude hiding              ((.), id)
 import System.Random
 
+-- Types
 data GCommand = Help
               | Quit
               | HM HMCommand
@@ -26,7 +22,8 @@ data GCommand = Help
 
 data HMCommand = Guess Char
                | Solve String
-               | New   String
+               | New
+               | Display
                deriving Show
 
 data Puzzle = Puzzle { puzzleString :: String
@@ -39,6 +36,11 @@ data Status = InProgress
             | Failure String
             deriving Show
 
+data PuzzleOut = Puzz Puzzle
+               | Swap Puzzle Puzzle
+               deriving Show
+
+-- Config
 wordlistFP :: FilePath
 wordlistFP = "data/wordlist.txt"
 
@@ -47,105 +49,153 @@ guesses = 7
 
 helpmsg :: String
 helpmsg = "Solve the hangman!  @new for a new game.  @solve [solution] to "
-       <> "solve.  @quit to quit.  Type any single character to guess that"
-       <> " character."
+       <> "solve.  @quit to quit.  @display to show current puzzle.  Type "
+       <> "any single character to guess that character."
 
+-- main
 main :: IO ()
 main = do
     putStrLn "Welcome to Hangman!  Type @help for help!"
     wordlist <- lines . map toLower <$> readFile wordlistFP
     g        <- getStdGen
-    _        <- interactIdI (hangman wordlist g)
+
+    -- pick an initial word and display it
+    let (str0, g') = pick wordlist g
+    putStrLn . display $ blankPuzzle str0
+
+    -- here we go
+    _  <- interactIdI (hangman wordlist g' str0)
+
     putStrLn "Goodbye!"
 
 hangman :: Monad m
-        => [String]
-        -> StdGen
+        => [String]     -- ^ Word list
+        -> StdGen       -- ^ Random seed
+        -> String       -- ^ Starting word
         -> Auto m String (Maybe String)
-hangman wordlist g = proc inp -> do
-
-    comm    <- case words inp of
-                 "@help" :_        -> id -< Just Help
-                 "@quit" :_        -> id -< Just Quit
-                 "@solve":ws       -> id -< Just $ HM (Solve (map toLower (unwords ws)))
-                 "@new"  :_        -> do
-                     w <- mkState_ (\_ -> pick wordlist) g -< ()
-                     id -< Just $ HM (New w)
-                 [[c]] | isAlpha c -> id -< Just $ HM (Guess (toLower c))
-                 _                 -> id -< Nothing
+hangman wordlist g str0 = proc inp -> do
+    -- Primitive command parser
+    let comm = case words inp of
+                 "@help"   :_      -> Just Help
+                 "@quit"   :_      -> Just Quit
+                 "@display":_      -> Just (HM Display)
+                 "@solve"  :ws     -> Just . HM . Solve
+                                    . map toLower . unwords $ ws
+                 "@new"    :_      -> Just (HM New)
+                 [[c]] | isAlpha c -> Just . HM . Guess . toLower $ c
+                 _                 -> Nothing
 
     case comm of
       Nothing         -> id -< return "Unknown command.  @help for help."
       Just Help       -> id -< return helpmsg
       Just Quit       -> id -< mzero
       Just (HM hcomm) -> do
-        puzz <- switchF game (game Nothing) -< hcomm
-        id    -< Just $ case puzz of
-                          Just p  -> display p
-                          Nothing -> "No game!"
+        -- Make a new random string, in case the puzzle needs it
+        newstr <- rand (pick wordlist) g -< ()
 
+        -- Puzzle, with the command and a fresh string if needed.
+        puzz   <- switchF game (game str0) -< (hcomm, newstr)
+
+        -- get wins and losses
+        swaps  <- blips isSwap -< puzz
+        losses <- countE . filterE isFailure -< swaps
+        wins   <- countE . filterE isSuccess -< swaps
+
+        -- display result
+        id      -< return $ case puzz of
+                              -- just the puzzle
+                              Puzz p     -> display p
+                              -- the old puzzle and a new puzzle
+                              Swap p0 p1 -> display p0
+                                         <> "\n"
+                                         <> "Wins: " <> show wins <> " | "
+                                         <> "Losses: " <> show losses
+                                         <> "\n"
+                                         <> display p1
 
 game :: Monad m
-     => Maybe String
-     -> Auto m HMCommand (Maybe Puzzle, Event (Maybe String))
-game mstr0 = proc comm ->
-    case (mstr0, comm) of
-      (_, New str1)  -> do
-        let newPuzz = Puzzle (map (mask "") str1) "" InProgress
-        new <- now -< Just str1
-        id -< (Just newPuzz, new)
+     => String
+     -> Auto m (HMCommand, String) (PuzzleOut, Event String)
+game str = proc (comm, newstr) -> do
+    -- get correct guesses, incorrect guesses, and solves
+    let (corr, incorr, solve) = case comm of
+            Guess c | c `elem` str -> (Just c , Nothing , False)
+                    | otherwise    -> (Nothing, Just c  , False)
+            Solve s | s == str     -> (Nothing, Nothing , True )
+                    | otherwise    -> (Nothing, Just '*', False)
+            _                      -> (Nothing, Nothing , False)
 
-      (Nothing, _)   -> do
-        e <- never -< ()
-        id -< (Nothing, e)
+    -- collect all correct guesses
+    rights <- mkAccum add " "            -< corr
 
-      (Just str, _) -> do
-        let (corr, incorr, solve) = case comm of
-                Guess c | c `elem` str -> (Just c , Nothing , False)
-                        | otherwise    -> (Nothing, Just c  , False)
-                Solve s | s == str     -> (Nothing, Nothing , True )
-                        | otherwise    -> (Nothing, Just '*', False)
-                _                      -> (Nothing, Nothing , False)
+        -- is it solved?
+    let solved = solve || all (`elem` rights) str
 
-        rights <- mkAccum add " "            -< corr
+        -- don't count it against the player if already solved
+        incorr'  | solved    = Nothing
+                 | otherwise = incorr
 
-        let solved = solve || all (`elem` rights) str
+    -- collect all wrong guesses
+    wrongs <- reverse <$> mkAccum add "" -< incorr'
 
-            wrong  | solved    = Nothing
-                   | otherwise = incorr
+        -- did the player run out of guesses?
+    let failed = length wrongs > guesses
 
-        wrongs <- reverse <$> mkAccum add "" -< wrong
+        -- make status
+        status | solved    = Success str
+               | failed    = Failure str
+               | otherwise = InProgress
 
-        let failed = length wrongs > guesses
+        -- the puzzle object
+        puzz   = Puzzle { puzzleString = map (mask rights) str
+                        , puzzleMisses = wrongs
+                        , puzzleStatus = status
+                        }
 
-            status | solved    = Success str
-                   | failed    = Failure str
-                   | otherwise = InProgress
+        -- Just p if there should be a new puzzle (from @new or game over)
+        mkNew  = case comm of
+                   New -> Just (puzz { puzzleStatus = Failure str })
+                   _   | solved || failed -> Just puzz
+                       | otherwise        -> Nothing
 
-            puzz   = Puzzle { puzzleString = map (mask rights) str
-                            , puzzleMisses = wrongs
-                            , puzzleStatus = status
-                            }
+    case mkNew of
+      -- new puzzle desired
+      Just p' -> do
+        let newPuzz = blankPuzzle newstr
+        new <- now -< newstr
+        id   -< (Swap p' newPuzz, new)
 
+      -- business as usual
+      Nothing -> do
         new <- never -< ()
-
-        id -< (Just puzz, new)
+        id   -< (Puzz puzz, new)
 
   where
+    -- add a unique element to a list.  but don't check for uniqueness if
+    -- '*' (a bad solve)
     add ws w = case w of
                  Just '*'                -> '*':ws
                  Just c | c `notElem` ws -> c  :ws
                  _                       -> ws
-    mask _ '_'              = ' '
-    mask rs c | c `elem` rs = c
-              | otherwise   = '_'
 
+-- pick random element from a list
 pick :: [a] -> StdGen -> (a, StdGen)
 pick [] _ = error "pick: Cannot pick from empty list."
 pick xs g = (xs !! n, g')
   where
     (n, g') = randomR (0, length xs - 1) g
 
+-- new blank puzzle
+blankPuzzle :: String -> Puzzle
+blankPuzzle str = Puzzle (map (mask "") str) "" InProgress
+
+-- mask item if in the given string
+mask :: String -> Char -> Char
+mask _ ' '              = ' '
+mask rs c | c `elem` rs = c
+          | otherwise   = '_'
+
+-- pretty-print a puzzle
 display :: Puzzle -> String
 display (Puzzle str m sts) = pre
                           <> " ["
@@ -159,3 +209,16 @@ display (Puzzle str m sts) = pre
                     InProgress -> ("Active:", str)
                     Success s  -> ("Solved!", s  )
                     Failure s  -> ("Failed!", s  )
+
+-- because no lens
+isSwap :: PuzzleOut -> Bool
+isSwap (Swap _ _)  = True
+isSwap _           = False
+
+isFailure :: PuzzleOut -> Bool
+isFailure (Swap (Puzzle _ _ (Failure _)) _) = True
+isFailure _                                 = False
+
+isSuccess :: PuzzleOut -> Bool
+isSuccess (Swap (Puzzle _ _ (Success _)) _) = True
+isSuccess _                                 = False
