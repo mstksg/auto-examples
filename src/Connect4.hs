7618f99ae7b03face2de7f1ba69547a287603677
Justin Le <justin@jle.im>
2014-07-21 04:03:11 -0700

    finishing up. now to comment.

--- src/Connect4.hs
+++ src/Connect4.hs
@@ -4,52 +4,72 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+-- | Usage:
+--
+-- $ connect4 [interface X] [interface O]
+--
+-- Interfaces:
+--   * h : human
+--   * cR: computer (random moves)
+--   * cE: computer (easy)
+--   * cH: computer (sorta hard)
+--
+-- Defaults to h vs cH
+--
+
 module Main (main) where
 
--- import Control.Auto.Generate
--- import Control.Auto.Run
--- import Control.Monad.IO.Class
--- import Data.Map.Strict           (Map)
-import Debug.Trace
-import Control.Auto hiding          (loop)
+import Control.Auto hiding             (loop)
 import Control.Auto.Blip
 import Control.Auto.Collection
-import Control.Auto.Interval hiding (when)
 import Control.Auto.Process.Random
 import Control.Auto.Switch
 import Control.Auto.Time
-import Control.Monad hiding         (forM_, mapM_)
+import Control.Monad
 import Control.Monad.Fix
-import Data.Foldable                (toList)
-import Data.Function hiding         ((.), id)
+import Data.Foldable                   (toList)
+import Data.Function hiding            ((.), id)
 import Data.Functor.Identity
 import Data.List
 import Data.Maybe
 import Data.Ord
 import Data.Serialize
 import GHC.Generics
-import Prelude hiding               ((.), id, mapM_)
+import Prelude hiding                  ((.), id, mapM_)
 import System.Console.ANSI
 import System.Environment
 import System.Random
-import qualified Data.Map.Strict    as M
+import qualified Data.Map.Strict       as M
 
-type Board = [[Piece]]
+-- Types
+type Board  = [Column]
+type Column = [Piece]
 type Player = Piece
 
+-- The Piece type, also representing a player.
 data Piece = X | O deriving (Show, Read, Eq, Ord, Generic)
 
+-- The output of the 'Board' Auto that allows the other Autos and stuff to
+--   display it and react to it.  Contains the board, the winner (Nothing
+--   if no winner, Just (Just p) if p won, and Just Nothing if a tie), the
+--   next player up, and if the last move failed.
 data BoardOut = BoardOut { _boBoard  :: !Board
                          , _boWinner :: !(Maybe (Maybe Player))
                          , _boNext   :: !Player
                          , _boFailed :: !Bool
                          } deriving Generic
 
+--   The type of the generic interfaces (human, CPU, etc).
 type Interface m = Auto m (Maybe Int, BoardOut) (Maybe Int)
+--                         ^          ^          ^
+--                         |          |          +-- Possible output
+--                         |          +-- Game output, for decision making
+--                         +-- Possible user input
 
 instance Serialize Piece
 instance Serialize BoardOut
 
+-- config, and initializations
 boardWidth, boardHeight :: Int
 boardWidth = 7
 boardHeight = 6
@@ -62,74 +82,244 @@ emptyBoardOut = BoardOut emptyBoard Nothing X False
 
 main :: IO ()
 main = do
+    -- build the two interfaces, from command line arguments.
     args <- getArgs
-    (i1, i2) <- case args of
+    (iX, iO) <- case args of
                   []      -> (,) <$> interfOf "h" <*> interfOf "cH"
-                  i1:[]   -> (,) <$> interfOf i1  <*> interfOf "cH"
-                  i1:i2:_ -> (,) <$> interfOf i1  <*> interfOf i2
-    putStrLn (showOut emptyBoardOut)
-    let a0 = game i1 i2
-    loop a0
+                  iO:[]   -> (,) <$> interfOf "h" <*> interfOf iO
+                  iX:iO:_ -> (,) <$> interfOf iX  <*> interfOf iO
+
+    -- The initial game Auto
+    let gameAuto = game iX iO
+
+    -- off we go!
+    loop gameAuto 0
   where
+    -- Build interfaces from command line arguments.
     interfOf :: MonadFix m => String -> IO (Interface m)
     interfOf "cH" = cpuAlphaBeta 8 <$> newStdGen
     interfOf "cE" = cpuAlphaBeta 4 <$> newStdGen
     interfOf "cR" = cpuRandom      <$> newStdGen
     interfOf _    = return human
 
-    loop a = do
-      i <- fromMaybe 0 . readMaybe <$> getLine
-      let Output bout a' = runIdentity (stepAuto a i)
+    -- the main loop
+    loop a i0 = do
+      let Output bout a' = runIdentity (stepAuto a i0)
       clearScreen
       putStrLn (showOut bout)
-      when (isNothing (_boWinner bout)) (loop a')
+      when (isNothing (_boWinner bout)) $ do
+        i1 <- fromMaybe 0 . readMaybe <$> getLine
+        loop a' i1
 
+-- the main game Auto.  Given two interfaces.
+--
+-- Interfaces are just a type alias for a normal Auto:
+--
+-- > type Interface m = Auto m (Maybe Int, BoardOut) (Maybe Int)
+--
+-- See the definition of the type for details on what each field means.
 game :: forall m. MonadFix m
-     => Interface m
-     -> Interface m
-     -> Auto m Int BoardOut
-game i1 i2 = fastForward Nothing game' <<^ Just
+     => Interface m           -- ^ X Player
+     -> Interface m           -- ^ O Player
+     -> Auto m Int BoardOut   -- ^ Game Auto
+     --        ^   ^
+     --        |   +-- Game output
+     --        +-- Player input
+-- game is the "fastForwarded" @game'@ Auto.
+--
+-- > fastFoward :: a -> Auto m a (Maybe b) -> Auto m a b
+--
+-- Basically, it takes a default value and an Auto returning 'Maybe b', and
+--   then, whenever that Auto is "run"/ticked, it repeatedly runs the Auto
+--   until it gets a 'Just' result.  It is initially run with the input,
+--   and then afterwards "fast forwarded" with the default input.  In
+--   essense, you get an Auto that "always returns 'Just'"...by "fast
+--   fowarding" over the 'Nothing's.  Like TiVo!
+--
+game iX iO = fastForward Nothing game' <<^ Just
   where
+    -- the fast-forwarded game Auto.  It feeds the input directly to the
+    --   Interface whose current turn it is, and then outputs a 'Just'
+    --   containing the resulting 'BoardOut' whenever an interface
+    --   "requests" player input.  Also outputs a 'Just' when the game is
+    --   over.
     game' :: Auto m (Maybe Int) (Maybe BoardOut)
+          --         ^           ^
+          --         |           +-- Board output, if player interaction is
+          --         |                 needed, or game is over.
+          --         +-- Player interaction, if this is a
+          --               "non-fastforwarded" tick.
     game' = proc i -> do
-      rec bout' <- delay emptyBoardOut -< bout
-          move <- mux interf -< (_boNext bout', (i, bout'))
-          bout <- board emptyBoard X -< fromMaybe 0 move
-      let outp = maybe (Just bout) (const Nothing) move
-      id -< outp <|> (bout <$ _boWinner bout)
-    interf X = i1
-    interf O = i2
+          -- the current BoardOut, bout, is the "last value" of newBout.
+          --   We will use this to give to our interfaces, so that they can
+          --   decide their next moves.
+      rec bout     <- lastVal emptyBoardOut -< newBout
+          -- feed (i, bout) (the player input, and the current board) to
+          --   the player playing next, _boNext bout.
+          --
+          -- 'mux' is an Auto multiplexer.   Give it an address/key
+          --   (_boNext bout, the next player), and it'll feed the input
+          --   (i, bout) to the Auto produced by that key, and output the
+          --   result.
+          --
+          -- For example, if you pass in (X, (Just 1, bout)), then it'll
+          --   pass in (Just 1, bout) to the X Auto.  'interf' is the
+          --   function that maps the key to the Auto, so the X Auto is
+          --   'interf X' = 'iX', the X interface.
+          move     <- mux interf            -< (_boNext bout, (i, bout))
+
+          -- feed the retrieved move into the Board auto.
+          newBout  <- board emptyBoard X    -< fromMaybe 0 move
+
+          -- the output of the Auto.
+      let output = case _boWinner newBout of
+                     -- if a winner, just report the new BoardOut
+                     Just _   -> Just newBout
+                     -- If no winner...
+                     Nothing -> case move of
+                                  -- if a move was received from an
+                                  --   Interface, all is good; no need to
+                                  --   output anything.  The "fast
+                                  --   forwarding" continues.
+                                  Just _  -> Nothing
+                                  -- if no move is received from an
+                                  --   interface, then we need to spark
+                                  --   some player interaction.  Return
+                                  --   a Just to "halt" the
+                                  --   fast-forwarding, and ask for input.
+                                  Nothing -> Just newBout
+
+      -- spit out the output.
+      id -< output
+
+    -- the correct interface for the player piece.
+    interf X = iX
+    interf O = iO
 
 
-board :: MonadFix m => Board -> Player -> Auto m Int BoardOut
+-- board: behave like 'board b0 p0' until a 'Blip' is received...then
+-- switch permanently to the 'gameOver' Auto.  See the 'hangman' example
+-- for more information on 'switchFromF'.
+board       :: MonadFix m
+            => Board    -- ^ initial Board
+            -> Player   -- ^ initial Player
+            -> Auto m Int BoardOut
+            --        ^   ^
+            --        |   +-- Resulting board
+            --        +-- Move to make
 board b0 p0 = switchFromF gameOver (board' b0 p0)
   where
+    -- gameOver: repeatedly just return the finished BoardOut, ignoring any
+    -- input...but every move is a failed move (of course).
     gameOver b = (pure b' &&& id) . never
       where
         b' = b { _boFailed = True }
 
-board' :: MonadFix m => Board -> Player -> Auto m Int (BoardOut, Blip BoardOut)
+-- The main Board Auto.
+board'       :: forall m. MonadFix m
+             => Board     -- ^ initial Board
+             -> Player    -- ^ initial Player
+             -> Auto m Int (BoardOut, Blip BoardOut)
+             --        ^    ^         ^
+             --        |    |         +-- switch to Game Over, with this
+             --        |    |               BoardOut.
+             --        |    +-- the resulting Board after the move
+             --        +-- the move to make.
 board' b0 p0 = proc i -> do
-    rec currP   <- mkAccum swapP p0 . delay False -< goodMove
-        brd     <- toList . fill <$> gather col -< (i, currP)
-        lastBrd <- delay b0 -< brd
+
+        -- wasGood: whether or not the previously attempted move was legal.
+    rec wasGood <- lastVal False                  -< goodMove
+
+        -- currP: the current player.  Will be "swapped" if the last move
+        --   was good, and kept the same if it wasn't.
+        currP   <- mkAccum swapP p0               -< wasGood
+
+        -- brd: the Board.  uses the 'gather' combinator, which is like the
+        --   'mux' combinator, but instead of outputting just the "currently
+        --   running/requested" Auto, it outputs the result of every Auto so
+        --   far.  Here, it holds every 'column' Auto, and runs the one given
+        --   by 'i' (the move).  Gives the 'column' Auto its input, a piece
+        --   ('currP').
+        --
+        -- (toList . fill) fills in the resulting Map as appropriate, and
+        --   then turns it back into a list of lists (a 'Board').
+        --
+        -- It needs to be filled because 'gather' "accumultes new columns",
+        --   as you enter in new moves.  So on the first move, if you put
+        --   a move in column 4, gather will be a Map with keys [4].  In
+        --   the next move, if you put a move in column 2, gather will
+        --   return a Map with keys [4,2].  'fill' fills in the gap and
+        --   replaces the missing keys/values in the map from the starting
+        --   columns in 'b0'.
+        brd     <- toList . fill <$> gather col   -< (i, currP)
+
+        -- lastBrd: The previous board, before the new move.
+        lastBrd <- lastVal b0                     -< brd
+
+        -- a good move is whether or not the previous board is the same as
+        --   the new, updated board (the move was not rejected)
         let goodMove = lastBrd /= brd
 
+    -- the winner.  Just (Just p) if p won, Just Nothing if a tie, and
+    --   Nothing if the game is still in progress.
     let winner | isWinner currP brd       = Just (Just currP)
                | length (concat brd) >= d = Just Nothing
                | otherwise                = Nothing
-    win <- onJusts -< winner
-    let boardOut = BoardOut brd winner (swapP currP goodMove) (not goodMove)
+
+    -- 'win' is a Blip that occurs as soon as 'winner' changes.  'winner'
+    --   is initially Nothing, so this basically means, it is emitted
+    --   whenever someone wins or there is a tie.
+    win <- onChange -< winner
+
+        -- the resulting BoardOut for this tick.
+    let boardOut = BoardOut brd
+                            winner
+                            (swapP currP goodMove)
+                            (not goodMove)
+
+    -- pop out the BoardOut, and the 'win', "tagged" with the BoardOut,
+    --   when it occurs.
     id -< (boardOut, boardOut <$ win)
   where
+    -- if in the right width
     inRange n = n > 0 && n <= length b0
+    -- number of tiles on the full board
     d         = boardHeight * boardWidth
+    -- fill the resulting map from 'gather' with the missing columns.  See
+    -- the binding of 'brd' for more information.
     fill      = flip M.union (M.fromList (zip [1..] b0))
-    col n | inRange n = Just <$> column (b0 !! (n - 1))
-          | otherwise = pure Nothing
+
+    -- the starting Auto for every column.  If the column number is not in
+    --   range, it is an always-Nothing.  If the column is, it's a 'column'
+    --   Auto, with starting confirguration given by b0 !! (n - 1).
+    --
+    -- 'gather' works by "pruning" 'Nothing' results, so the out-of-range
+    --   columns are instantly pruned, and the in-range columns are forever
+    --   a part of 'gather'.
+    col       :: Int -> Auto m Piece (Maybe Column)
+    col n     | inRange n = Just <$> column (b0 !! (n - 1))
+              | otherwise = pure Nothing
+    -- swap the player, if the Bool 's' ("if the last move was good or
+    --   not") is True.
     swapP p s | s         = opp p
               | otherwise = p
 
+-- the actual 'column' Auto, of which every 'board' is composed out of.
+--   This is a basic usage of 'mkAccum', which is sort of like an Auto
+--   foldl.  Give the initial accumulator, and a merging function, apply
+--   the function to every incoming item and the accumulator to get a new
+--   accumulator and output value.
+--
+-- In our case, we simply add the new incoming piece to the accumulator (a
+--   column), and then 'take' only the amount that we need, keeping the
+--   height of the column at most 'boardHeight'.
+column :: Monad m => Column -> Auto m Piece Column
+column = mkAccum (\ps p -> take boardHeight (ps ++ [p]))
+
+
+-- Utilities
+
+-- check for winner
 isWinner :: Player -> Board -> Bool
 isWinner p b = (any . any) hasFour [ filled , transpose filled
                                    , wedgeUp, wedgeDown
@@ -143,8 +333,7 @@ isWinner p b = (any . any) hasFour [ filled , transpose filled
     wedgeUp   = transpose $ zipWith (++) wedge filled
     wedgeDown = transpose $ zipWith (++) (reverse wedge) filled
 
--- Utilities
-
+-- show the Board
 showBoard :: Board -> String
 showBoard = unlines   . map concat
           . transpose . map fill
@@ -152,6 +341,7 @@ showBoard = unlines   . map concat
     fill :: [Piece] -> [String]
     fill = map (++ "|") . reverse . take boardHeight . (++ repeat "_") . map show
 
+-- show a BoardOut
 showOut :: BoardOut -> String
 showOut (BoardOut brd winner nextP _) =
     unlines [ unwords (map show [1..boardWidth])
@@ -163,13 +353,12 @@ showOut (BoardOut brd winner nextP _) =
                                               Nothing -> "Tie game."
             ]
 
-column :: Monad m => [Piece] -> Auto m Piece [Piece]
-column = mkAccum (\ps p -> take boardHeight (ps ++ [p]))
-
+-- swap a piece/player.
 opp :: Piece -> Piece
 opp X = O
 opp O = X
 
+-- read, possibly failing with a 'Nothing'.
 readMaybe :: Read a => String -> Maybe a
 readMaybe = fmap fst . listToMaybe . reads
 
@@ -200,16 +389,14 @@ cpuRandom g = Just <$> stdRands (randomR (1, boardWidth)) g
 -- to try out --- some sort of "retry" ?
 cpuAlphaBeta :: MonadFix m => Int -> StdGen -> Interface m
 cpuAlphaBeta lim g = proc (_, bout) -> do
-    rec lastGl    <- delay (BIn 0) -< gl
-        lastRetry <- delay False   -< retry
+    rec lastRetry <- lastVal False -< retry
 
         let currP = _boNext bout
             bo0   = _boBoard bout
             a0    = board bo0 currP
-            retry = not (lastRetry) && lastGl == BMin
-            lim'  | retry     = 2
+            retry = gl == BMin && not lastRetry
+            lim'  | lastRetry = 2
                   | otherwise = min (length (concat bo0) * 2) lim
-            -- lim'  = min (length (concat bo0) * 2) lim
 
         weights <- accelerate boardWidth (stdRands random g) -< ()
 
