0a1637e6be053939d3b78054537576f8fa4bf807
Justin Le <justin@jle.im>
2014-07-30 04:22:43 -0700

    tweaks to test performance

--- src/Adventure.hs
+++ src/Adventure.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import Data.Map.Strict (Map)
+
+type RoomID = Int
+
+data Room = Room { roomID    :: RoomID
+                 , roomName  :: String
+                 , roomDesc  :: String
+                 , roomItems :: [Item]
+                 , roomLinks :: Map String RoomID
+                 }
+
+data Item = Item
+
 main :: IO ()
 main = putStrLn "adventure time!"
+
+
--- src/MHMC.hs
+++ src/MHMC.hs
@@ -3,11 +3,11 @@
 module Main (main) where
 
 import Control.Auto
-import Control.Auto.Process.Random
-import Control.Monad
-import Control.Auto.Time
-import Control.Auto.Time
 import Control.Auto.Collection
+import Control.Auto.Generate
+import Control.Auto.Process.Random
+import Control.Auto.Time
+import Control.Monad
 import Control.Monad.Fix
 import Data.Functor.Identity
 import Debug.Trace
@@ -22,9 +22,9 @@ targetDist x = exp (-((x - 1)**2)) / sqrt pi * 25
 
 -- TODO: adjust dx to match acceptance rate
 
-mhmc :: MonadFix m => Double -> Double -> StdGen -> Auto m a (Double, Double)
+mhmc :: MonadFix m => Double -> Double -> StdGen -> Auto m a (Double, Int)
 mhmc x0 dx g = proc _ -> do
-    rec jump <- stdRands (randomR (-dx, dx)) g1 -< ()
+    rec jump <- rands_ (randomR (-dx, dx)) g1 -< ()
 
         let x'    = x + jump
             p     = targetDist x
@@ -32,19 +32,35 @@ mhmc x0 dx g = proc _ -> do
             jProb | p' > p    = 1
                   | otherwise = exp (p' - p)
 
-        toJump <- stdRands (randomR (0, 1)) g2 -< ()
+        toJump <- rands_ (randomR (0, 1)) g2 -< ()
 
         let takeJump = toJump < jProb
 
-        x <- delay x0 -< if takeJump then x'
-                                     else x
+        x <- delay_ x0 -< if takeJump then x'
+                                      else x
 
-    jumps <- mkAccum (+) 0 -< if takeJump then 1 else 0
-    total <- fromIntegral . (+ 1) <$> count -< ()
+    jumps <- mkAccum_ (+) 0 -< if takeJump then 1 else 0
 
-    id -< (x, jumps / total)
+    id -< (x, jumps)
   where
     (g1, g2) = split g
+
+mhmc' :: Monad m => Double -> Double -> StdGen -> Auto m a (Double, Int)
+mhmc' x0 dx g0 = mkState_ (const f) (x0, 0, g0)
+  where
+    f (x, jC, g) = let (jump, g')    = randomR (-dx, dx) g
+                       x'            = x + jump
+                       p             = targetDist x
+                       p'            = targetDist x'
+                       jProb         | p' > p    = 1
+                                     | otherwise = exp (p' - p)
+                       (toJump, g'') = randomR (0, 1) g'
+                       takeJump      = toJump < jProb
+                       jC'           | takeJump  = jC + 1
+                                     | otherwise = jC
+                       x''           | takeJump  = x'
+                                     | otherwise = x
+                       in  ((x, jC), (x'', jC', g''))
 
 
 main :: IO ()
