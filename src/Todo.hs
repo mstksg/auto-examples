3992b762cd5a624085be39039dc3c4604516e1ae
Justin Le <justin@jle.im>
2015-02-17 00:24:12 -0800

    documented the todo example

--- src/Todo.hs
+++ src/Todo.hs
@@ -22,33 +22,93 @@ import Prelude hiding          ((.), id, interact)
 import Text.Read
 import qualified Data.Map      as M
 
-(<&>) :: Functor f => f a -> (a -> b) -> f b
-(<&>) = flip fmap
+type TaskID = Int
 
-data InpEvent = IEAdd String
-              | IETask Int TaskEvent
+-- | An Input event, from the GUI
+data InpEvent = IEAdd  String
+              | IETask TaskID TaskCmd
               deriving Show
 
-data TaskEvent = TEDelete
-               | TEComplete Bool
-               | TEModify String
-               deriving Show
+-- | Describing a task command
+data TaskCmd = TEDelete
+             | TEComplete Bool
+             | TEModify String
+             deriving Show
 
+-- | A single task
 data Task = Task { taskDescr     :: Maybe String
                  , taskCompleted :: Bool
                  } deriving (Show, Generic)
 
 instance Serialize Task
 
-taskMap :: Monad m => Auto m (Int, TaskEvent) (Map Int Task)
-taskMap = gather (const mkTask)
+-- | The main Auto.  Takes in a Blip stream of input events, and outputs
+-- a Map of TaskId's and Tasks.
+taskInp :: MonadFix m => Auto m (Blip InpEvent) (Map TaskID Task)
+taskInp = proc inpEvtB -> do
+
+        -- the previous taskMap
+    rec ids <- arrD M.keys [] -< tMap
+
+        -- "forking" the blip stream
+        -- * one blip stream for new task blips, filtering on isAdd
+        newTaskB <- perBlip newTask . mapMaybeB isAdd -< inpEvtB
+        -- * one blip stream for task mod blips, filtering on validTE
+        modTaskB <- mapMaybeB validTE                 -< (ids,) <$> inpEvtB
+
+        -- re-join them back together with `mergeL`
+        let tmInpB = newTaskB `mergeL` modTaskB
+
+        -- a Map of Task Id's and Tasks, running `taskMap` per emitted
+        -- input
+        tMap <- holdWith mempty . perBlip taskMap -< tmInpB
+
+    id -< tMap
   where
-    mkTask = mkAccum f (Just (Task Nothing False))
+    -- Used with `mapMaybeB` to filter the stream on valid task commands
+    validTE (ids, IETask n te) | n `elem` ids = Just (n, te)
+    validTE _                                 = Nothing
+
+    -- Used to increment id's and create a new task
+    newTask = proc descr -> do
+      newId <- count -< ()
+      id -< (newId, TEModify descr)
+
+-- | An Auto that takes a TaskID and a Task command and updates an internal
+-- map of TaskID's to Tasks, using `gather`.  See documentation for help on
+-- `gather`.
+--
+-- Basically, `gather` keeps a `Map` of `k`s to `Interval a b`s (An
+-- `Interval a b` is just an `Auto a (Maybe b)`.  Give a `(k, a)` as an
+-- input, and it feeds the `a` to the `Auto` at `k`.  Every step, outputs
+-- a full map of the last emitted value from every `Auto`.
+--
+-- Here, it emits, at every step, the full map of all tasks and their
+-- statuses.  The internal map is a map of Autos representing each task.
+-- We can "update" a task by feeding in a tuple with the Task ID we want to
+-- update, and the TaskCmd we want the task auto to receive.
+--
+-- The Task Auto can "delete itself" by outputting `Nothing`.
+taskMap :: Monad m => Auto m (TaskID, TaskCmd) (Map TaskID Task)
+taskMap = gather (const taskAuto)
+  where
+    -- the Auto for each individual task: fold over the folding function
+    -- `f` for each input, with the current task.  Use `Nothing` to signal
+    -- that it wants to delete itself.
+    taskAuto = mkAccum f (Just (Task Nothing False))
     f _        TEDelete       = Nothing
     f (Just t) (TEComplete c) = Just (t { taskCompleted = c        })
     f (Just t) (TEModify str) = Just (t { taskDescr     = Just str })
     f t        _              = t
 
+
+-- | Used with `mapMaybeB` to filter the stream on adding blips
+isAdd :: InpEvent -> Maybe String
+isAdd (IEAdd descr) = Just descr
+isAdd _             = Nothing
+
+-- | Parse a string input.  Just for testing.  Ideally, these events will
+-- come from a GUI.
 parseInp :: String -> Maybe InpEvent
 parseInp = p . words
   where
@@ -58,37 +118,14 @@ parseInp = p . words
     p ("U":n:_)  = readMaybe n <&> \n' -> IETask n' (TEComplete False)
     p ("M":n:xs) = readMaybe n <&> \n' -> IETask n' (TEModify (unwords xs))
     p _          = Nothing
-
-taskInp :: MonadFix m => Auto m String (Map Int Task)
-taskInp = proc inpStr -> do
-    inpEvtB <- emitJusts parseInp -< inpStr
-
-    rec lastTM <- lastVal mempty -< taskMap
-
-        let ids    = M.keys lastTM
-
-        newTaskB <- perBlip newTask . mapMaybeB isAdd -< inpEvtB
-        modTaskB <- mapMaybeB validTE                 -< (ids,) <$> inpEvtB
-
-        let tmInpB = newTaskB `mergeL` modTaskB
-
-        taskMap <- hold . perBlip taskMap <|!> pure mempty -< tmInpB
-
-    id -< taskMap
-  where
-    validTE (ids, IETask n te) | n `elem` ids = Just (n, te)
-    validTE _                                 = Nothing
-
-    newTask = proc descr -> do
-      newId <- count -< ()
-      id -< (newId, TEModify descr)
+    (<&>) :: Functor f => f a -> (a -> b) -> f b
+    (<&>) = flip fmap
 
 
-isAdd :: InpEvent -> Maybe String
-isAdd (IEAdd descr) = Just descr
-isAdd _             = Nothing
 
-formatTodo :: Map Int Task -> String
+-- | Just for command line testing use, turning the Map into a String.
+-- Ideally this would be handled by a GUI.
+formatTodo :: Map TaskID Task -> String
 formatTodo = unlines . map format . M.toList
   where
     format (n, Task desc compl) = concat [ show n
@@ -100,3 +137,4 @@ formatTodo = unlines . map format . M.toList
 
 main :: IO ()
 main = void . interact $ fmap (Just . formatTodo) taskInp
+                       . emitJusts parseInp
